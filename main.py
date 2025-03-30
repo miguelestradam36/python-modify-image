1ebe796c09ab259d16a762ddc9369f94c2f470ff
miguelestradam36 <miguelestradam36@gmail.com>
2025-03-29 18:00:00 -0600

    Uploading some additional methods to the project

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from pillow_modules.imagemodifier import ImageManager
 
 if __name__ == "__main__":
     Image_modifier = ImageManager()
-    Image_modifier.transpose_image(image_path="example.png", output_name="output.png")
+    #Image_modifier.transpose_image_left(image_path="example.png", output_name="output.png")
+    #Image_modifier.flip_image_down(image_path="example.png", output_name="output_1.png")
+    Image_modifier.change_size(image_path="example.png", output_name="output_2.png", size_left=200, size_right=300)
     del Image_modifier
 
 
--- pillow_modules/imagemodifier.py
+++ pillow_modules/imagemodifier.py
@@ -2,12 +2,30 @@ class ImageManager():
     def __init__(self):
         print("Starting the transformation of images...")
 
-    def transpose_image(self, image_path:str, output_name:str)->None:
+    def transpose_image_left(self, image_path:str, output_name:str)->None:
         from PIL import Image
         Image.open(image_path)
         output_image = Image.open(image_path)
         mirror_image = output_image.transpose(Image.FLIP_LEFT_RIGHT)
         mirror_image.save(output_name)
+    
+    def flip_image_down(self, image_path:str, output_name:str)->None:
+        from PIL import Image
+        Image.open(image_path)
+        output_image = Image.open(image_path)
+        mirror_image = output_image.transpose(Image.FLIP_TOP_BOTTOM)
+        mirror_image.save(output_name)  
+
+    def change_size(self, image_path:str, output_name:str, size_left:int, size_right:int)->None:
+        from PIL import Image, ImageOps
+        size = (size_left, size_right)
+        with Image.open(image_path) as im:
+            ImageOps.contain(im, size).save(output_name)
+    
+    def change_contrast(self, image_path:str, contrast:float)->None:
+        from PIL import ImageEnhance
+        enh = ImageEnhance.Contrast(image_path)
+        enh.enhance(contrast).show("{}% more contrast".format(contrast))
 
     def __del__(self):
         print("Finished the image transformation...")
